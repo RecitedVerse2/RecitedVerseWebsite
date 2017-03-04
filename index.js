eaa284b70c08d5bd139cf97c45b6d45bdb80f1e6
Adeola Uthman <authman2@gmail.com>
2017-03-03 21:18:26 -0500

    updatse

--- index.js
+++ index.js
@@ -11,14 +11,10 @@ app.set('view engine', 'ejs');
 
 // The different pages of the app, in the form "/your_page_extension (ex.) /about, /profile, etc.)"
 app.get('/', function(request, response) {
-  response.render('pages/landing');
+  response.render('pages/landing.ejs');
 });
 
 
-//app.listen(app.get('port'), function() {
-//  console.log('Node app is running on port', app.get('port'));
-//});
-
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
