c0c1e9b90e1d2ef243119d931c9780cdaf25a094
Adeola Uthman <authman2@gmail.com>
2017-03-07 13:17:51 -0500

    More login work

---
+++ public/stylesandscripts/login.js
@@ -0,0 +1,59 @@
+/* Get the objects. */
+var emailField = document.getElementById('email_field');
+var passwordField = document.getElementById('password_field');
+var statusLabel = document.getElementById('status_label');
+var loginBtn = document.getElementById('login_btn');
+
+
+
+/**
+    Sign in to the user's account.
+*/
+loginBtn.onclick = function() {
+    var email = emailField.value;
+    var password = passwordField.value;
+    
+    // Sign in.
+    fireAuth.signInWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        
+        if(errorCode === 'auth/wrong-password' || errorCode === 'auth/invalid-email') {
+            statusLabel.innerHTML = "Incorrect Email or Password.";
+            statusLabel.style.visibility = "visible";
+            return;   
+        }
+    });
+    
+    // Display status
+    statusLabel.style.visibility = "visible";
+    statusLabel.style.color = "green";
+    statusLabel.innerHTML = "Signing in!";
+
+    // Save the user object.
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            
+            fireRef.child('Users').child(user.uid).once('value').then(function(snapshot) {
+                var email = snapshot.val()["email"];
+                var fullname = snapshot.val()["fullname"];
+                var password = snapshot.val()["password"];
+                var userID = snapshot.val()["userID"];
+                var photoURL = snapshot.val()["photoURL"];
+                
+                currentUser = {
+                    "fullname" : fullname,
+                    "email" : email,
+                    "password" : password,
+                    "userID" : user.uid,
+                    "photoURL" : photoURL
+                };
+                console.log(currentUser);
+            });
+            // Go to the home page.
+            //document.location = "";
+        } else {}
+    });
+
+};
