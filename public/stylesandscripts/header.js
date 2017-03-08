2febea905eaa94872b826d1152cd8bc31abf8090
Adeola Uthman <authman2@gmail.com>
2017-03-08 15:57:29 -0500

    Menu button actions

--- public/stylesandscripts/header.js
+++ public/stylesandscripts/header.js
@@ -2,6 +2,14 @@ var menuBtn = $('.open_menu_btn');
 var searchBar = $('.rv_searchbar');
 var menu = $('.menu');
 
+var homeBtn = $('#home_btn');
+var albumsBtn = $('#albums_btn');
+var artistsBtn = $('#artists_btn');
+var genresBtn = $('#genres_btn');
+var myAccountBtn = $('#myaccount_btn');
+var goToLoginBtn = $('#goTo_login_btn');
+var goToRegisterBtn = $('#goTo_register_btn');
+
 
 // When the user taps the enter key on the search bar.
 searchBar.keyup(function(event) {
@@ -36,4 +44,27 @@ menuBtn.click(function() {
         searchBar.addClass('rv_searchbar');
         searchBar.removeClass('rv_searchbar_expanded');
     }
+});
+
+// Each menu button.
+homeBtn.click(function() {
+    document.location = "https://recitedverse.herokuapp.com/home";
+});
+albumsBtn.click(function() {
+    document.location = "https://recitedverse.herokuapp.com/albums";
+});
+artistsBtn.click(function() {
+    document.location = "https://recitedverse.herokuapp.com/artists";
+});
+genresBtn.click(function() {
+    document.location = "https://recitedverse.herokuapp.com/genres";
+});
+myAccountBtn.click(function() {
+    document.location = "https://recitedverse.herokuapp.com/profile";
+});
+goToLoginBtn.click(function() {
+    document.location = "https://recitedverse.herokuapp.com/login";
+});
+goToRegisterBtn.click(function() {
+    document.location = "https://recitedverse.herokuapp.com/signup";
 });
