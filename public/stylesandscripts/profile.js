4ca1b1df5e43b57ea52dac45f36c2c2dd988365c
Adeola Uthman <authman2@gmail.com>
2017-03-09 11:59:31 -0500

    Mostly worked out loading the current user

--- public/stylesandscripts/profile.js
+++ public/stylesandscripts/profile.js
@@ -1,9 +1,9 @@
 /* All of the parts of the profile. */
 var profilePicture = document.getElementById('profile_picture');
-var nameLabel = document.getElementById('#profile_name');
-var bioLabel = document.getElementById('#profile_bio');
-var followersLabel = document.getElementById('#profile_followers');
-var followingLabel = document.getElementById('#profile_following');
+var nameLabel = document.getElementById('profile_name');
+var bioLabel = document.getElementById('profile_bio');
+var followersLabel = document.getElementById('profile_followers');
+var followingLabel = document.getElementById('profile_following');
 
 
 if (typeof(Storage) !== "undefined") {
