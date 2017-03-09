f7b854ef85b7cfe9b16d3aa43cfd9cb8a0f95721
Adeola Uthman <authman2@gmail.com>
2017-03-09 11:54:07 -0500

    Mostly worked out loading the current user

--- public/stylesandscripts/profile.js
+++ public/stylesandscripts/profile.js
@@ -6,15 +6,10 @@ var followersLabel = $('#profile_followers');
 var followingLabel = $('#profile_following');
 
 
-/* Once the DOM is loaded, fill in all of the user information.
-This assumes that currentUser is already initialized. */
-$(document).ready(function() {
+
+currentUser = loadCurrentUser();
     
-    currentUser = window.loadCurrentUser();
-    
-    nameLabel.value = currentUser["fullname"];
-    bioLabel.value = currentUser["bio"];
-    followersLabel.val = "Followers: " + currentUser["followers"];
-    followingLabel.val = "Following: " + currentUser["following"]; 
-    
-});
+nameLabel.value = currentUser["fullname"];
+bioLabel.value = currentUser["bio"];
+followersLabel.val = "Followers: " + currentUser["followers"];
+followingLabel.val = "Following: " + currentUser["following"]; 
