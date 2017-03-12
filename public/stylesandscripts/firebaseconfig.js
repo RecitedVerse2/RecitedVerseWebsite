c36ddd20b451df8e1407e7bb02d80810d3ea216b
Adeola Uthman <authman2@gmail.com>
2017-03-12 00:14:09 -0500

    updates

--- public/stylesandscripts/firebaseconfig.js
+++ public/stylesandscripts/firebaseconfig.js
@@ -3,7 +3,7 @@ var config = {
     apiKey: "AIzaSyAliB2PpmncS1gNmKtOoIeQ4LgC74cpFUw",
     authDomain: "recitedverse-6efe4.firebaseapp.com",
     databaseURL: "https://recitedverse-6efe4.firebaseio.com",
-    storageBucket: "gs://recitedverse-6efe4.appspot.com",
+    storageBucket: "recitedverse-6efe4.appspot.com",
     messagingSenderId: "1078860066504"
 };
 firebase.initializeApp(config);
