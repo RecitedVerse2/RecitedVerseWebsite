9bd4555960a7c648270f8ef54c54e240671f219f
Adeola Uthman <authman2@gmail.com>
2017-03-15 23:01:53 -0400

    working on custom alert

--- public/stylesandscripts/profile.js
+++ public/stylesandscripts/profile.js
@@ -87,7 +87,7 @@ function CustomAlert() {
         dialogoverlay.style.display = "block";
         dialogoverlay.style.height = winH+"px";
         //dialogbox.style.left = (winW/2) - (400 * 0.4)+"px";
-        dialogbox.style.left = "50%";
+        dialogbox.style.left = "50% - (400/2)";
         dialogbox.style.top = "100px";
         dialogbox.style.display = "block";
     }
