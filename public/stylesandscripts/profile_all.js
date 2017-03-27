4c0d46c128406c1d7e2b223412973c74eac73c73
Adeola Uthman <authman2@gmail.com>
2017-03-27 17:37:03 -0400

    Laying out the recitations

--- public/stylesandscripts/profile_all.js
+++ public/stylesandscripts/profile_all.js
@@ -14,7 +14,7 @@ fireRef.child('Recitations').child(currentUser["userID"]).on('value', function(s
         if(listOfObjects.hasOwnProperty(recitation)) {
             var recitationObject = listOfObjects[recitation];
             
-            var item = "<li class='recitation_item' style='font-size:15px;'><img id='recitation_img' width='120' height='120' src='" + recitationObject.image + "' alt='image'><button id='goToPoemPageBtn' style='color:black;' onclick='if (typeof(Storage) !== "undefined") {window.sessionStorage.setItem('recitation_to_look_at', JSON.stringify(recitation)); document.location = 'https://recitedverse.herokuapp.com/poem';}'>" + recitationObject.title + "</button></li>";
+            var item = "<li class='recitation_item' style='font-size:15px;'><img id='recitation_img' width='120' height='120' src='" + recitationObject.image + "' alt='image'><button id='goToPoemPageBtn' style='color:black;' onclick='if (typeof(Storage) !== "undefined") {window.sessionStorage.setItem('recitation_to_look_at', JSON.stringify(recitationObject)); document.location = 'https://recitedverse.herokuapp.com/poem';};'>" + recitationObject.title + "</button></li>";
             recList.append(item);
         }
     }
