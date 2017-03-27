7a0b6cd3f6ecd4592d30da97d282273a33a8a097
Adeola Uthman <authman2@gmail.com>
2017-03-27 17:54:49 -0400

    Laying out the recitations

--- public/stylesandscripts/profile_all.js
+++ public/stylesandscripts/profile_all.js
@@ -14,7 +14,7 @@ fireRef.child('Recitations').child(currentUser["userID"]).on('value', function(s
         if(listOfObjects.hasOwnProperty(recitation)) {
             var recitationObject = listOfObjects[recitation];
             
-            var item = "<li class='recitation_item' style='font-size:15px;'><img id='recitation_img_" + recitationObject.name + "' width='120' height='120' src='" + recitationObject.image + "' alt='image'><button class='goToBtn' id='goToPoemPageBtn_" + recitationObject.name + "' style='color:black;' onclick='goToPoemPageWithRecitation(recitationObject)'>" + recitationObject.title + "</button></li>";
+            var item = "<li class='recitation_item' style='font-size:15px;'><img id='recitation_img_" + recitationObject.name + "' width='120' height='120' src='" + recitationObject.image + "' alt='image'><button class='goToBtn' id='goToPoemPageBtn_" + recitationObject.name + "' style='color:black;' onclick='goToPoemPageWithRecitation(" + recitationObject + ")'>" + recitationObject.title + "</button></li>";
             recList.append(item);
             
 //            var s1 = 'recitation_img_' + recitationObject.name;
