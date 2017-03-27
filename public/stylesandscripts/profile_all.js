cf36aed8eb8cff62c6a23668dcc176772973c267
Adeola Uthman <authman2@gmail.com>
2017-03-27 19:09:41 -0400

    Laying out the recitations

--- public/stylesandscripts/profile_all.js
+++ public/stylesandscripts/profile_all.js
@@ -14,6 +14,9 @@ fireRef.child('Recitations').child(currentUser["userID"]).on('value', function(s
         if(listOfObjects.hasOwnProperty(recitation)) {
             var recitationObject = listOfObjects[recitation];
             
+            console.log(recitationObject);
+            console.log(listOfObjects[recitation]);
+            
             var item = "<li class='recitation_item' style='font-size:15px;'><img id='recitation_img_" + recitationObject.name + "' width='120' height='120' src='" + recitationObject.image + "' alt='image'><button class='goToBtn' id='goToPoemPageBtn_" + recitationObject.name + "' style='color:black;'>" + recitationObject.title + "</button></li>";
             console.log("ITEM: " + item);
             recList.append(item);
