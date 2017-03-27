e8e94e31e1c4791f81b3a15b4663b098a890b711
Adeola Uthman <authman2@gmail.com>
2017-03-27 19:25:03 -0400

    Trying to fix clicking on recitation

--- public/stylesandscripts/profile_all.js
+++ public/stylesandscripts/profile_all.js
@@ -14,18 +14,15 @@ fireRef.child('Recitations').child(currentUser["userID"]).on('value', function(s
         if(listOfObjects.hasOwnProperty(recitation)) {
             var recitationObject = listOfObjects[recitation];
             
-            console.log( listOfObjects[recitation]["name"] );
-            
-            var item = "<li class='recitation_item' style='font-size:15px;'><img id='recitation_img_" + recitationObject.name + "' width='120' height='120' src='" + recitationObject.image + "' alt='image'><button class='goToBtn' id='goToPoemPageBtn_" + recitationObject.name + "' style='color:black;'>" + recitationObject.title + "</button></li>";
-            //console.log("ITEM: " + item);
+            var item = "<li class='recitation_item' style='font-size:15px;'><img id='recitation_img_" + recitationObject.title + "' width='120' height='120' src='" + recitationObject.image + "' alt='image'><button class='goToBtn' id='goToPoemPageBtn_" + recitationObject.title + "' style='color:black;'>" + recitationObject.title + "</button></li>";
             recList.append(item);
             
-            var s1 = 'recitation_img_' + recitationObject.name;
-            var s2 = 'goToPoemPageBtn_' + recitationObject.name;
+            var s1 = 'recitation_img_' + recitationObject.title;
+            var s2 = 'goToPoemPageBtn_' + recitationObject.title;
             
-//            console.log(s1);
-//            console.log(s2);
-//            
+            console.log(s1);
+            console.log(s2);
+            
             var recImage = document.getElementById(s1);
             var goToBtn = document.getElementById(s2);
             recImage.onclick = function() {
