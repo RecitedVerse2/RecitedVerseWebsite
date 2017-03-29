ccb67f3b24911fb839a812a3a76b45e7bcbb2d1e
Adeola Uthman <authman2@gmail.com>
2017-03-29 12:15:24 -0400

    Liking/Favoriting recitations

--- public/stylesandscripts/poem.js
+++ public/stylesandscripts/poem.js
@@ -21,93 +21,111 @@ if (typeof(Storage) !== "undefined") {
     recitation = JSON.parse(window.sessionStorage.getItem("recitation_to_look_at"));
     
     // Load the current version of it from firebase.
+    loadCurrentUser(function() {
+        loadRecitation(function() {
+            
+            /* SET VARIABLES */
+            imageView.src = recitation["image"];
+            titleAuthorArea.innerHTML = recitation["title"] + " by " + recitation["author"];
+            recitedByArea.innerHTML = "Recited by " + recitation["recited_by"] + ", Published: " + recitation["published"] + ", Genre: " + recitation["genre"];
+            descriptionArea.innerHTML = recitation["description"];
+
+            playLabel.innerHTML = recitation["plays"];
+            likeLabel.innerHTML = recitation["likes"];
+            favoriteLabel.innerHTML = recitation["favorites"];
+
+
+
+
+            /* FUNCTIONS */
+            function remove(arr) {
+                var what, a = arguments, L = a.length, ax;
+                while (L > 1 && arr.length) {
+                    what = a[--L];
+                    while ((ax= arr.indexOf(what)) !== -1) {
+                        arr.splice(ax, 1);
+                    }
+                }
+                return arr;
+            }
+
+
+            playBtn.onclick = function() {
+
+            };
+
+            likeBtn.onclick = function() {
+                var s = recitation["title"] + " " + recitation["author"];
+                var arr = currentUser["likes"];
+
+                if(!arr.includes(s)) {
+                    recitation.likes += 1;
+                    likeLabel.innerHTML = recitation["likes"];
+                    currentUser["likes"].push(s);
+
+                    // Save to firebase.
+                    fireRef.child("Recitations").child(currentUser["userID"]).child(recitation["title"]).set(recitation);
+                    fireRef.child("Users").child(currentUser["userID"]).update(currentUser);
+                    return;
+                } else {
+                    recitation.likes -= 1;
+                    likeLabel.innerHTML = recitation["likes"];
+                    remove(currentUser["likes"], s);
+
+                    // Save to firebase.
+                    fireRef.child("Recitations").child(currentUser["userID"]).child(recitation["title"]).set(recitation);
+                    fireRef.child("Users").child(currentUser["userID"]).update(currentUser);
+                    return;
+                }
+            };
+
+            favoriteBtn.onclick = function() {
+                var s = recitation["title"] + " " + recitation["author"];
+                var arr = currentUser["favorites"];
+
+                // If it's already there, remove it. If not, add it.
+                if(!arr.includes(s)) {
+                    recitation.favorites += 1;
+                    favoriteLabel.innerHTML = recitation["favorites"];
+                    currentUser["favorites"].push(s);
+
+                    // Save to firebase.
+                    fireRef.child("Recitations").child(currentUser["userID"]).child(recitation["title"]).set(recitation);
+                    fireRef.child("Users").child(currentUser["userID"]).update(currentUser);
+                    return;
+                } else {
+                    recitation.favorites -= 1;
+                    favoriteLabel.innerHTML = recitation["favorites"];
+                    remove(currentUser["favorites"], s);
+
+                    // Save to firebase.
+                    fireRef.child("Recitations").child(currentUser["userID"]).child(recitation["title"]).set(recitation);
+                    fireRef.child("Users").child(currentUser["userID"]).update(currentUser);
+                    return;
+                }
+            };
+        });
+    });
+};
+
+
+
+
+function loadCurrentUser(callback) {
+    fireRef.child('Users').child(currentUser["userID"]).once('value', function(snapshot) {
+        var userObj = snapshot.val();
+        currentUser = userObj;
+    
+        callback();
+    });
+};
+
+
+function loadRecitation(callback) {
     fireRef.child('Recitations').child(currentUser["userID"]).child(recitation["title"]).once('value', function(snapshot) {
         var recObject = snapshot.val();
         recitation = recObject;
-        console.log(recitation);
-    
-    
-        /* SET VARIABLES */
-        imageView.src = recitation["image"];
-        titleAuthorArea.innerHTML = recitation["title"] + " by " + recitation["author"];
-        recitedByArea.innerHTML = "Recited by " + recitation["recited_by"] + ", Published: " + recitation["published"] + ", Genre: " + recitation["genre"];
-        descriptionArea.innerHTML = recitation["description"];
-
-        playLabel.innerHTML = recitation["plays"];
-        likeLabel.innerHTML = recitation["likes"];
-        favoriteLabel.innerHTML = recitation["favorites"];
-
-
-
-
-        /* FUNCTIONS */
-
-        function remove(arr) {
-            var what, a = arguments, L = a.length, ax;
-            while (L > 1 && arr.length) {
-                what = a[--L];
-                while ((ax= arr.indexOf(what)) !== -1) {
-                    arr.splice(ax, 1);
-                }
-            }
-            return arr;
-        }
-
-
-        playBtn.onclick = function() {
-
-        };
-
-        likeBtn.onclick = function() {
-            var s = recitation["title"] + " " + recitation["author"];
-            var arr = currentUser["likes"];
-
-            if(!arr.includes(s)) {
-                recitation.likes += 1;
-                likeLabel.innerHTML = recitation["likes"];
-                currentUser["likes"].push(s);
-
-                // Save to firebase.
-                fireRef.child("Recitations").child(currentUser["userID"]).child(recitation["title"]).set(recitation);
-                fireRef.child("Users").child(currentUser["userID"]).update(currentUser);
-                return;
-            } else {
-                recitation.likes -= 1;
-                likeLabel.innerHTML = recitation["likes"];
-                remove(currentUser["likes"], s);
-
-                // Save to firebase.
-                fireRef.child("Recitations").child(currentUser["userID"]).child(recitation["title"]).set(recitation);
-                fireRef.child("Users").child(currentUser["userID"]).update(currentUser);
-                return;
-            }
-        };
-
-        favoriteBtn.onclick = function() {
-            var s = recitation["title"] + " " + recitation["author"];
-            var arr = currentUser["favorites"];
-
-            // If it's already there, remove it. If not, add it.
-            if(!arr.includes(s)) {
-                recitation.favorites += 1;
-                favoriteLabel.innerHTML = recitation["favorites"];
-                currentUser["favorites"].push(s);
-
-                // Save to firebase.
-                fireRef.child("Recitations").child(currentUser["userID"]).child(recitation["title"]).set(recitation);
-                fireRef.child("Users").child(currentUser["userID"]).update(currentUser);
-                return;
-            } else {
-                recitation.favorites -= 1;
-                favoriteLabel.innerHTML = recitation["favorites"];
-                remove(currentUser["favorites"], s);
-
-                // Save to firebase.
-                fireRef.child("Recitations").child(currentUser["userID"]).child(recitation["title"]).set(recitation);
-                fireRef.child("Users").child(currentUser["userID"]).update(currentUser);
-                return;
-            }
-        };
         
+        callback();
     });
 };
