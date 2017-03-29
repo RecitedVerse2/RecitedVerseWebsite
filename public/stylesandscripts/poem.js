9dab86605827ed4a983f6cbbf7acc042712969db
Adeola Uthman <authman2@gmail.com>
2017-03-29 09:09:44 -0400

    Populating the poem page

--- public/stylesandscripts/poem.js
+++ public/stylesandscripts/poem.js
@@ -1,26 +1,27 @@
+/* GET VARIABLES */
+var imageView = document.getElementById('poem_image');
+var titleAuthorArea = document.getElementById('title_author_area');
+var recitedByArea = document.getElementById('recBy_Pub_Gen');
+var descriptionArea = document.getElementById('descr_area');
+
+var playBtn = document.getElementById('description_play_button');
+var likeBtn = document.getElementById('description_like_button');
+var favoriteBtn = document.getElementById('description_favorite_button');
+
+var playLabel = document.getElementById('poem_play_label');
+var likeLabel = document.getElementById('poem_like_label');
+var favoriteLabel = document.getElementById('poem_favorite_label');
+
+
+
+
 if (typeof(Storage) !== "undefined") {
     currentUser = JSON.parse(window.localStorage.getItem("current_user"));
     
     var recitation = JSON.parse(window.sessionStorage.getItem("recitation_to_look_at"));
     console.log(recitation);
-    
-    
-    /* GET VARIABLES */
-    
-    var imageView = document.getElementById('poem_image');
-    var titleAuthorArea = document.getElementById('titleAuthorArea');
-    var recitedByArea = document.getElementById('recBy_Pub_Gen');
-    var descriptionArea = document.getElementById('descr_area');
-    
-    var playBtn = document.getElementById('description_play_button');
-    var likeBtn = document.getElementById('description_like_button');
-    var favoriteBtn = document.getElementById('description_favorite_button');
-    
-    var playLabel = document.getElementById('poem_play_label');
-    var likeLabel = document.getElementById('poem_like_label');
-    var favoriteLabel = document.getElementById('poem_favorite_label');
-    
-    
+    console.log(recitation["author"]);
+    console.log(recitation.author);
     
     
     /* SET VARIABLES */
