4e45949a158f31a66eff25d1b16e48fc1dfabd12
Adeola Uthman <authman2@gmail.com>
2017-04-03 23:45:05 -0400

    Added more to upload modal

--- src/components/ProfilePageComps/UploadBox.js
+++ src/components/ProfilePageComps/UploadBox.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import ReactModal from 'react-modal';
 
+import _ from '../../css/UploadBox.css';
+
+
 // The box for uploading recitations.
 class UploadBox extends Component {
 
@@ -44,19 +47,46 @@ class UploadBox extends Component {
                 </div>
                 <br/>
 
-                <h5>Poem Name: <input type="text" placeholder="Enter the name of the poem"/> </h5>
-                <h5>Author: <input type="text" placeholder="Enter the author's name"/> </h5>
-                <h5>Recited By: <input type="text" placeholder="Enter the reciter's name"/> </h5>
-                <h5>Published: <input type="text" placeholder="Year of publication"/> </h5>
-                <h5>Genre: <input type="text" placeholder="Enter the genre of the poem"/> </h5>
+                <h5>Poem Name: <input className='inp' type="text" placeholder="Enter the name of the poem"/> </h5>
+                <h5>Author: <input className='inp'  type="text" placeholder="Enter the author's name"/> </h5>
+                <h5>Recited By: <input className='inp'  type="text" placeholder="Enter the reciter's name"/> </h5>
+                <h5>Published: <input className='inp'  type="text" placeholder="Year of publication"/> </h5>
+                <h5>Genre: <input className='inp'  type="text" placeholder="Enter the genre of the poem"/> </h5>
                 <h5>Written Text: </h5>
-                <textarea rows="15" cols="35"></textarea>
-                <h5>Description (Optional): </h5>
+                <textarea className='inp'  rows="15" cols="35"></textarea>
+                <h5 className='inp' >Description (Optional): </h5>
                 <textarea rows="5" cols="30"></textarea>
                 <h5>Image (Optional):</h5>
                 <img width="200px" height="200px" src="https://firebasestorage.googleapis.com/v0/b/recitedverse-6efe4.appspot.com/o/RV_Website%2FEmptyPhoto.png?alt=media&token=ce1a33f5-f1d6-4f22-a6f8-8ab40cbd5c83" alt="poem_photo"/>
                 <br/><br/>
 
+                <form style={{textAlign: 'center'}}>
+                    <input type="file" name="recImage" id="recImageFile" className="inputfile" accept="image/x-png" multiple="false" />
+                    <label id="addPhotoBtn" className="pill_btn" htmlFor="recImageFile">Add Photo</label>
+                </form>
+                <br />
+                <h5 className="page_text">Upload a recording from a file</h5>
+                <form style={{textAlign: 'center'}}>
+                    <input type="file" name="fileRec" id="fileRecitation" className="inputfile" accept="audio/x-mpeg" multiple="false" />
+                    <label id="fromFileBtn" className="pill_btn" htmlFor="fileRecitation">Upload</label>
+                    <p id="filenameLabel" style={{color: 'limegreen'}} />
+                </form>
+                <p className="page_text">Or</p>
+                <h5 className="page_text">Record a recitation here.</h5>
+                <p id="canvas_holder" className="canvas_holder"><canvas className="visualizer" /></p>
+                <div className="mediaButtons">
+                    <button id="recordBtn" className="roundBtn fa fa-microphone" />
+                    <button id="stopRecBtn" className="roundBtn fa fa-stop-circle-o" />
+                    <button id="playAudioBtn" className="roundBtn fa fa-play" />
+                    <button id="pauseBtn" className="roundBtn fa fa-pause" />
+                    <button id="stopBtn" className="roundBtn fa fa-stop" />
+                    <button id="clearBtn" className="roundBtn fa fa-trash" />
+                </div>
+
+                <br /><br /><br /><br />
+                <div className="modal-footer">
+                    <button className="pill_btn" id="submit_recitation_btn" style={{cursor:'pointer'}}>Submit</button>
+                </div>
             </ReactModal>
         );
     }
