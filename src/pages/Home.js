d07f5cade9567b9633951e2a03a027b1407b6545
jakeols <jake_ols@live.com>
2018-04-12 12:07:47 -0700

    100% width

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -29,7 +29,8 @@ class Home extends Component {
         return {
             position:'absolute',
             left:'0px',
-            top:'0px'
+            top:'0px',
+            width: '100%',
         };
     }
     getOverlay() {
