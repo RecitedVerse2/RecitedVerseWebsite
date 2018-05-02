3589932672fee800a7f2143bdda6b99353a1ee56
jakeols <jake_ols@live.com>
2018-05-02 14:21:26 -0700

    setup / functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,18 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+const algoliasearch = require('algoliasearch');
+// const client = algoliasearch(functions.config().algolia.app_id, functions.config().algolia.api_key);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const ALGOLIA_POSTS_INDEX_NAME = 'recitedverse';
+
+
+// trigger for recitations
+exports.userCreated = functions.database.ref('/Recitations').onWrite((data, context) => {
+
+});
+
+// trigger for users
+exports.userCreated = functions.database.ref('/Users').onWrite((data, context) => {
+
+});
