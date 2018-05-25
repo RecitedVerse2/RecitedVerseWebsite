86e006ce6d96b150052eb3113b1f5a0b7832a529
jakeols <jake_ols@live.com>
2018-05-25 13:03:46 -0700

    functions add to algolia

--- functions/index.js
+++ functions/index.js
@@ -2,17 +2,29 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 const algoliasearch = require('algoliasearch');
-// const client = algoliasearch(functions.config().algolia.app_id, functions.config().algolia.api_key);
+const client = algoliasearch("I5KQF3O5KB", "42033f4e31521dd84c71ca19f4c9c744");
 
 const ALGOLIA_POSTS_INDEX_NAME = 'recitedverse';
 
 
 // trigger for recitations
-exports.userCreated = functions.database.ref('/Recitations').onWrite((data, context) => {
-
+exports.recitationCreated = functions.database.ref('/Recitations/{pushId}/').onCreate((snapshot, context) => {
+    const index = client.initIndex(ALGOLIA_POSTS_INDEX_NAME);
+    const original = snapshot.val();
+    const firebaseObject = {
+      recitation: original,
+    };
+  
+    return index.saveObject(firebaseObject);
 });
 
 // trigger for users
-exports.userCreated = functions.database.ref('/Users').onWrite((data, context) => {
-
+exports.userCreated = functions.database.ref('/Users/{pushId}').onCreate((data, context) => {
+    const index = client.initIndex(ALGOLIA_POSTS_INDEX_NAME);
+    const original = snapshot.val();
+    const firebaseObject = {
+      user: original,
+    };
+  
+    return index.saveObject(firebaseObject);
 });
