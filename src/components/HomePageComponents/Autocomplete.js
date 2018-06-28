806a540739f800c9d746b9fcd074417d46d3df97
jakeols <jake_ols@live.com>
2018-06-28 12:45:50 -0700

    searchbox works

--- src/components/HomePageComponents/Autocomplete.js
+++ src/components/HomePageComponents/Autocomplete.js
@@ -11,43 +11,46 @@ import { connectStateResults } from "react-instantsearch/connectors";
 import _ from '../../css/fonts.css';
 import __ from '../../css/Header.css';
 
+const Product = ({ hit }) => {
+  return (
+      <div style={{width: '100%'}} key={hit.recitation.title} onClick={() => handleLinkClick(hit)}>
+        <span style={{width: '100%', cursor: 'pointer' }}>{hit.recitation.title} | {hit.recitation.uploaderName}</span>
+      <hr />
+      </div>
+  );
+};
+
+const Search = () => {
+  return (
+    <div>
+      <SearchBox />
+      <ul className="suggestions" id="sugg">
+      <Results />
+      </ul>
+    </div>
+  );
+};
+
+const handleLinkClick = (hit) => {
+  window.open(`/user?${hit.recitation.uploaderID}`);
+}
+
+
+const Results = connectStateResults(
+  ({ searchState }) =>
+    searchState && searchState.query ? (
+      <Hits hitComponent={Product} />
+    ) : //<div>No query</div>
+    null
+);
+
 export default class Autocomplete extends Component {
     constructor(props){
         super(props);
-        this.handleLinkClick = this.handleLinkClick.bind(this);
-    }
-    handleLinkClick(hit){
-        window.open(`/user?${hit.recitation.uploaderID}`);
     }
     render(){
 
-        const Product = ({ hit }) => {
-            return (
-                <div style={{width: '100%'}} key={hit.recitation.title} onClick={() => this.handleLinkClick(hit)}>
-                  <span style={{width: '100%', cursor: 'pointer' }}>{hit.recitation.title} | {hit.recitation.uploaderName}</span>
-                <hr />
-                </div>
-            );
-          };
-          
-          const Results = connectStateResults(
-            ({ searchState }) =>
-              searchState && searchState.query ? (
-                <Hits hitComponent={Product} />
-              ) : //<div>No query</div>
-              null
-          );
-          
-          const Search = () => {
-            return (
-              <div>
-                <SearchBox />
-                <ul className="suggestions" id="sugg">
-                <Results />
-                </ul>
-              </div>
-            );
-          };
+
 
         return (
             <div style={{width: '100%'}} >
